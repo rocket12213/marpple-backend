97e174dc6592d4b1be9617315473e95e95e42c6a
devzunky <devzunky@gmail.com>
2019-11-21 19:38:34 +0900

    Social Login test completed
    Social Login test를 완성했습니다.

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+import jwt
+import json
+import unittest
 
-# Create your tests here.
+from django.test   import Client, TestCase
+from user.models   import User, SocialPlatform
+from unittest.mock import patch, MagicMock
+
+class KakaoSignInTest(TestCase):
+
+    def setUp(self):
+        SocialPlatform.objects.create(platform_name='kakao')
+
+    def tearDown(self):
+        SocialPlatform.objects.get(platform_name='kakao').delete()        
+    
+    @patch('user.views.requests')
+    def test_kakao_sign_in(self, mocked_request):
+        c = Client()
+
+        class FakeResponse:
+            def json(self):
+                return {
+                    "id" : 12345,
+                    "properties" : {"nickname": "test_user"},
+                    "kakao_account": {"email":"test@gmail.com"}
+                }
+
+        mocked_request.get = MagicMock(return_value = FakeResponse())
+        header = {'HTTP_Authorization':'fake_token.1234'}
+        response = c.get('/user/kakao-login', content_type='applications/json', **header)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
